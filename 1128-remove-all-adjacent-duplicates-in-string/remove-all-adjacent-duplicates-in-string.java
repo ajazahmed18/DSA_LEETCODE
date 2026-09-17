cd98788e088dc5c7087672c3134628f5ff6917e3
MOHAMMAD AJAZ AHMED <ajazmd8055@gmail.com>
2026-09-17 19:04:10 +0530

    Time: 124 ms (10.07%) | Memory: 49.1 MB (6.10%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(stack.isEmpty()) {
+                stack.push(c);
+            }
+            else if(c == stack.peek()) {
+                stack.pop();
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        String ans = "";
+        for(char c : stack) {
+            ans += c;
+        }
+        return ans;
+    }
+}
