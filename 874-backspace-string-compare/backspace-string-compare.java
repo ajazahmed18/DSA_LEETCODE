31390982f4a1b3626e58bf75a801bad2da4a47fd
MOHAMMAD AJAZ AHMED <ajazmd8055@gmail.com>
2026-09-17 18:38:51 +0530

    Time: 2 ms (59.70%) | Memory: 43.2 MB (28.76%) - LeetSync

---
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class Solution {
+    public boolean backspaceCompare(String s, String t) {
+        Stack<Character> s1 = new Stack<>();
+        Stack<Character> t1 = new Stack<>();
+        // for s
+        for(char c : s.toCharArray()) {
+            if(c == '#') {
+                if(!s1.isEmpty()) {
+                    s1.pop();
+                }
+            }
+            else {
+                s1.push(c);
+            }
+        }
+        // for t
+        for(char c : t.toCharArray()) {
+            if(c == '#') {
+                if(!t1.isEmpty()) {
+                    t1.pop();
+                }
+            }
+            else {
+                t1.push(c);
+            }
+        }
+        return s1.equals(t1);
+    }
+}
