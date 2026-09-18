b205719fbee4ddc0bf68e16dccf1dce57a77681e
MOHAMMAD AJAZ AHMED <ajazmd8055@gmail.com>
2026-09-18 19:59:48 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (15.91%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+
+        ArrayList<String> ans = new ArrayList<>();
+        int j = 0;
+        for(int i = 1; i <= n; i++) {
+            ans.add("Push");
+
+            if(target[j] == i) {
+                j++;
+            }
+            else {
+                ans.add("Pop");
+            }
+            if(j == target.length) {
+                break;
+            }
+        }
+        return ans;
+    }
+}
